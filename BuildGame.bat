46bdd228c97dbe184b97c35c137db30d87919ba3
Kiliken <131953633+Kiliken@users.noreply.github.com>
2025-10-16 18:03:47 +0900

    Change output executable name from Main.exe to App.exe

--- BuildGame.bat
+++ BuildGame.bat
@@ -8,7 +8,7 @@ SET includes=/I src /I %cd%/SFML-xVersioNx/include
 SET links=/link /LIBPATH:%cd%/src /LIBPATH:%cd%/SFML-xVersioNx/lib MSVCRT.lib LIBCMT.lib opengl32.lib User32.lib Shell32.lib Gdi32.lib Kernel32.lib Advapi32.lib Ole32.lib Oleaut32.lib uuid.lib odbc32.lib odbccp32.lib sfml-system.lib sfml-graphics.lib sfml-window.lib sfml-audio.lib freetype.lib winmm.lib flac.lib vorbisenc.lib vorbisfile.lib vorbis.lib ogg.lib ws2_32.lib
 SET defines=/D RELEASE
 
-cl /std:c++17 /EHsc /Fe"%cd%/build/Main.exe" /Fo"%cd%/build/" %includes% %defines% src/main.cpp %links%
+cl /std:c++17 /EHsc /Fe"%cd%/build/App.exe" /Fo"%cd%/build/" %includes% %defines% src/*.cpp %links%
 
 if %errorlevel% neq 0 (
 		color 0c
@@ -17,6 +17,6 @@ if %errorlevel% neq 0 (
 		color 0a
 		echo Build succeeded. Launching game...
 		cd build
-		Main.exe
+		App.exe
 )
 
